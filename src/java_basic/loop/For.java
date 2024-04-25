81838935e5fae0ea8c67639fa7e779ac10285655
wowba <leeyuwk54@gmail.com>
2024-04-26 08:56:11 +0900

    자바 반복문 마무리

---
+++ src/java_basic/loop/For.java
@@ -0,0 +1,18 @@
+package java_basic.loop;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        int count = 0;
+        for (int i = 1; i < 10; i++) { // i는 for 문 내에서만 사용 가능
+            if (count > 10) {
+                break;
+            }
+            count = count + i;
+        }
+        System.out.println(count);
+    }
+}
